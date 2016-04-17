c37f4ced891421c63f066a0dbac86feaa689c360
y_jono <pek9dkl@gmail.com>
2016-04-17 20:20:12 +0900

    Initial Commit

---
+++ SampleHealthKitApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleHealthKitApp
+//
+//  Created by Seino Yoshinori on 2016/04/17.
+//  Copyright © 2016年 yoshinori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
